86125cd2c9d47963a9f8552acb882b6c739b8482
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-23 23:27:07 +0530

    Refactor reverseString function implementation

--- reversestring04.cpp
+++ reversestring04.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-
-// Function to reverse the string using a stack
 void reverseString(string &str) {
-
     stack<char> st;
-
-    // Push each character of the string into the stack
     for(int i = 0; i < str.length(); i++) {
         st.push(str[i]);
     }
-
-    // Pop characters from the stack and overwrite the string
     for(int i = 0; i < str.length(); i++) {
         str[i] = st.top();
         st.pop();
@@ -20,16 +13,11 @@ void reverseString(string &str) {
 }
 
 int main() {
-
     string str;
-
     cout << "Enter a string: ";
     cin >> str;
-
     reverseString(str);
-
     cout << "Reversed String: " << str << endl;
-
     return 0;
 }
 //output
