e2d4be3d849790043cf8a62fcf27951cd75337df
Md Imran Shakeel <imshkl2005@gmail.com>
2026-09-12 10:33:56 +0530

    Update Stackintro01.cpp

--- Stackintro01.cpp
+++ Stackintro01.cpp
@@ -102,95 +102,62 @@ int main() {
 
 #include <iostream>
 using namespace std;
-
-// Node of Linked List
 class Node {
 public:
-    int data;       // Stores stack element
-    Node* next;     // Points to next node
-
+    int data;      
+    Node* next;     
     Node(int d) {
         data = d;
         next = NULL;
     }
 };
 
-// Stack using Linked List
 class Stack {
-
-    Node* top;       // Points to top of stack
+    Node* top;      
 
 public:
-
-    // Constructor
     Stack() {
         top = NULL;
     }
-
-    // Push an element onto the stack
     void push(int x) {
-
-        // Create new node
         Node* newNode = new Node(x);
-
-        // New node points to current top
         newNode->next = top;
-
-        // Move top to new node
         top = newNode;
     }
-
-    // Remove top element
     void pop() {
-
-        // Stack is empty
         if(top == NULL) {
             cout << "Stack Underflow\n";
             return;
         }
-
-        // Store current top
         Node* temp = top;
-
-        // Move top to next node
         top = top->next;
-
-        // Delete old top node
         delete temp;
     }
 
-    // Return top element
+   
     int peek() {
-
         if(top == NULL) {
             cout << "Stack is Empty\n";
             return -1;
         }
-
         return top->data;
     }
 
-    // Check whether stack is empty
     bool isEmpty() {
-
         return top == NULL;
     }
 
-    // Print stack
+    
     void display() {
-
         if(top == NULL) {
             cout << "Stack is Empty\n";
             return;
         }
-
         Node* temp = top;
-
         while(temp != NULL) {
             cout << temp->data << " ";
             temp = temp->next;
         }
-
         cout << endl;
     }
 };
